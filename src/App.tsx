06286bec930431044b3fd5d02c34401a26b5cede
Sebastian Cerviño <cervisebas@gmail.com>
2025-05-06 12:22:09 -0300

    feat: update example

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,78 @@
-import { View, Window, Text, Button, Image, toPixmapFile } from "@cervisebas/react-nodegui";
+import React, { useRef, useState } from "react";
+import { View, Window, Text, Button, Image, toPixmapFile, Dialog, RNWindow } from "@cervisebas/react-nodegui";
 import IconAsset from "./assets/nodegui.png";
 import { QIcon } from "@nodegui/nodegui";
 
 const winIcon = new QIcon(toPixmapFile(IconAsset));
-const minSize = {
+const minSizeWindow = {
   width: 400,
   height: 480,
 };
 
+const sizeDialog = {
+  width: 240,
+  height: 120,
+};
+
 export function App() {
+  const [showDialog, setShowDialog] = useState(false);
+  const [windowSize, setWindowSize] = useState('0x0');
+  const refWindow = useRef<RNWindow>(null);
+
   return (
-    <Window
-      minSize={minSize}
-      windowIcon={winIcon}
-      styleSheet={styleSheet}
-      windowTitle={process.title}
-    >
-      <View id={'container'}>
-        <Image
-          id={'logo'}
-          src={IconAsset}
-          size={{
-            width: 100,
-            height: 100,
-          }}
-        />
-
-        <Text id={'title'}>
-          React NodeGUI!
-        </Text>
-
-        <View id={'button_container'}>
-          <Button
-            id={'button'}
-            text={'Open dialog'}
-            on={{
-              clicked() {
-                console.log('Hi');
-              },
+    <React.Fragment>
+      <Window
+        ref={refWindow}
+        minSize={minSizeWindow}
+        windowIcon={winIcon}
+        styleSheet={styleSheet}
+        windowTitle={process.title}
+      >
+        <View id={'container'}>
+          <Image
+            id={'logo'}
+            src={IconAsset}
+            size={{
+              width: 100,
+              height: 100,
             }}
           />
+
+          <Text id={'title'}>
+            React NodeGUI!
+          </Text>
+
+          <View id={'button_container'}>
+            <Button
+              id={'button'}
+              text={'Ver tamaño de ventana'}
+              on={{
+                clicked() {
+                  const size = refWindow.current?.native.size();
+
+                  setWindowSize(`${size?.width()}x${size?.height()}`);
+                  setShowDialog(true);
+                },
+              }}
+            />
+          </View>
         </View>
-      </View>
-    </Window>
+      </Window>
+
+      <Dialog
+        visible={showDialog}
+        windowIcon={winIcon}
+        windowTitle={'Test dialog'}
+        size={sizeDialog}
+        minSize={sizeDialog}
+        maxSize={sizeDialog}
+        styleSheet={dialogStyleSheet}
+      >
+        <View id={'container'}>
+          <Text id={'title'}>Tamaño: {windowSize}</Text>
+        </View>
+      </Dialog>
+    </React.Fragment>
   );
 }
 
@@ -62,5 +92,17 @@ const styleSheet = `
   #logo {
     width: 100px;
     height: 100px;
+  }
+`;
+
+const dialogStyleSheet = `
+  #container {
+    flex: 1;
+    justify-content: 'center';
+    align-items: 'center';
+  }
+  #title {
+    font-size: 18px;
+    font-weight: bold;
   }
 `;
